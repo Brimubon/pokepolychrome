a5f7674132d2f9a03879dedb8bc758a8166f3949
Brimubon <johnsoneve754@gmail.com>
2025-03-03 17:48:04 -0500

    Post-1.11.0 fixes/changes

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -43,9 +43,9 @@
 #define TEMP_FLAGS_END   FLAG_TEMP_1F
 #define NUM_TEMP_FLAGS   (TEMP_FLAGS_END - TEMP_FLAGS_START + 1)
 
-#define FLAG_UNUSED_0x020    0x20 // Unused Flag
-#define FLAG_UNUSED_0x021    0x21 // Unused Flag
-#define FLAG_UNUSED_0x022    0x22 // Unused Flag
+#define FLAG_DEXNAV_SEARCH   0x20 // dexnav search flag
+#define FLAG_DEXNAV_GET      0x21 // dexnav added to start menu flag
+#define FLAG_DETECTOR_MODE   0x22 // enable dexnav detector mode
 #define FLAG_UNUSED_0x023    0x23 // Unused Flag
 #define FLAG_UNUSED_0x024    0x24 // Unused Flag
 #define FLAG_UNUSED_0x025    0x25 // Unused Flag
